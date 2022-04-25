5d357a868cfae457dd77d925a2e04ce584398241
TOGU <45081449+ToguDV@users.noreply.github.com>
2022-04-24 23:00:35 -0500

    Add PropertyController

---
+++ RealState/Controllers/PropertyItemsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealState.Entities;
+using RealState.Services;
+
+namespace RealState.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyItemsController : ControllerBase
+    {
+        private readonly RealStateDbContext _context;
+
+        public PropertyItemsController(RealStateDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PropertyItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PropertyItem>>> GetPropertyItems()
+        {
+            return await _context.PropertyItems.ToListAsync();
+        }
+
+        // GET: api/PropertyItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PropertyItem>> GetPropertyItem(int id)
+        {
+            var propertyItem = await _context.PropertyItems.FindAsync(id);
+
+            if (propertyItem == null)
+            {
+                return NotFound();
+            }
+
+            return propertyItem;
+        }
+
+        // PUT: api/PropertyItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPropertyItem(int id, PropertyItem propertyItem)
+        {
+            if (id != propertyItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(propertyItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PropertyItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PropertyItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PropertyItem>> PostPropertyItem(PropertyItem propertyItem)
+        {
+            _context.PropertyItems.Add(propertyItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPropertyItem", new { id = propertyItem.Id }, propertyItem);
+        }
+
+        // DELETE: api/PropertyItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePropertyItem(int id)
+        {
+            var propertyItem = await _context.PropertyItems.FindAsync(id);
+            if (propertyItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.PropertyItems.Remove(propertyItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PropertyItemExists(int id)
+        {
+            return _context.PropertyItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ RealState/Migrations/20220425034518_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RealState.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PropertyItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    CodeInternal = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    IdOwner = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PropertyItems");
+        }
+    }
+}
